563c673c58133d0d36e92e7e1f6c2ab14c618eeb
Noya Samesh <noya.samesh@post.runi.ac.il>
2025-11-25 21:53:03 +0200

    complet

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -172,6 +172,10 @@ public class ArrCharOps {
         if (str1 == null || str2 == null){
             return -2;
         }
+        if (str1.length() == 0 && str2.length() == 0) return 0;
+        if (str1.length() == 0) return -1;
+        if (str2.length() == 0) return 1;
+
         int minLen = Math.min(str1.length(), str2.length());
 
         for (int i = 0; i < minLen; i++) {
