adf4c763b6fa71ddab53a64e24c6b5c275471c4d
Noya Samesh <noya.samesh@post.runi.ac.il>
2025-11-25 22:17:24 +0200

    complet

--- Primes.java
+++ Primes.java
@@ -1,27 +1,28 @@
 public class Primes {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        boolean [] isPrime = new boolean[n];
-        for (int i=2; i<n; i++){
+        boolean [] isPrime = new boolean[n +1];
+        for (int i=2; i<=n; i++){
             isPrime[i]= true;
         }
         int p=2;
-        while (p * p < n){
+        while (p * p <= n){
             if(isPrime[p]){
-                for (int i= p*p; i<n; i+=p){
+                for (int i= p*p; i<=n; i+=p){
                     isPrime[i] = false;
                 }
             }
             p++;
         }
+
         int count=0;
-        for (int i=2; i<n; i++){
+        for (int i=2; i<=n; i++){
             if (isPrime[i]==true){
                 System.out.println(i);
                 count++;
             }
         }
         int precentage = (count * 100) / n;
-        System.out.println("There are "+ count + " primes between 2 and " + n + " ("+ precentage +"% are primes)");
+        System.out.println("There are " + count + " primes between 2 and " + n + " (" + precentage + "% are primes)");
     }
 }
