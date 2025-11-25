386184d1effad316f4be90fa0bd6d4502f2c597c
Noya Samesh <noya.samesh@post.runi.ac.il>
2025-11-25 22:18:45 +0200

    complet

--- Primes.java
+++ Primes.java
@@ -15,6 +15,8 @@ public class Primes {
             p++;
         }
 
+        System.out.println("Prime numbers up to " + n + ":");
+
         int count=0;
         for (int i=2; i<=n; i++){
             if (isPrime[i]==true){
