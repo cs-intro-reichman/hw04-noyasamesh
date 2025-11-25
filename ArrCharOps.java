65b0732b77eb895718bf45d9530b4ee1ef262aba
Noya Samesh <noya.samesh@post.runi.ac.il>
2025-11-25 22:04:41 +0200

    complet

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -169,23 +169,28 @@ public class ArrCharOps {
      *         return -2 if there is an error with the input.
      */
     public static int compareTo(String str1, String str2) {
-        if (str1 == null || str2 == null){
-            return -2;
-        }
+        if (str1 == null && str2 == null) {
+        return 0;
+    }
+    if (str1 == null) {
+        return -1;
+    }
+    if (str2 == null) {
+        return 1;
+    }
     
-        int minLen = Math.min(str1.length(), str2.length());
-
-        for (int i = 0; i < minLen; i++) {
-          char c1 = str1.charAt(i);
-          char c2 = str2.charAt(i);
-            if (c1 != c2) {
-               if (c1 < c2) {
+    int minLen = Math.min(str1.length(), str2.length());
+    for (int i = 0; i < minLen; i++) {
+        char c1 = str1.charAt(i);
+        char c2 = str2.charAt(i);
+        if (c1 != c2) {
+            if (c1 < c2) {
                 return -1;
-               } else {
+            } else {
                 return 1;
-               }
             }
         }
+    }
     
     if (str1.length() == str2.length()) {
         return 0;
